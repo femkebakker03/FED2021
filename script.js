aa7a008ac616fa3beb683648cd1bd9990507a7d5
femkebakker03 <94450906+femkebakker03@users.noreply.github.com>
2022-01-26 20:02:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-// function voor als je op de button drukt naar boven te gaan //
+var button = document.querySelector(".button4 button")
+var menuButton = document.querySelector(".nav svg:first-of-type")
+var menu = document.querySelector(".nav > section")
 
+// function voor als je op de button drukt naar boven te gaan //
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+button.addEventListener("click", topFunction)
+
+function menuOpen() {
+    menu.classList.toggle("menuOpen")
+}
+
+menuButton.addEventListener("click", menuOpen)
+menu.addEventListener("click", menuOpen)
